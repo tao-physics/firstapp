f9602431f31174ba465580f4f5aca61eedf9f31d
Tao Ren <taoren.physics@icloud.com>
2016-10-25 16:09:37 -0700

    Initial Commit

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-	return "Hello, World!"
+	return "Hello, World! Hello, Tao!"
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
